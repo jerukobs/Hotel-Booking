ad24ec2f8725e80245b61bf2f5a45f5ac72c7b0f
Elvis Agbesi <elvisagbesi@gmail.com>
2019-04-03 12:58:29 +0000

    added announcements and event section. \n navigation bar floats

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+
+
 function myfunc() {
    alert("Well done");
 } 
+
+
+window.onscroll = function () {
+   console.log("c");
+   stickyfunction();
+}
+
+// get nav bar
+var navbar = document.getElementById("nav_b");
+
+// get offset position of navbar
+var sticky = navbar.offsetTop;
+
+
+function stickyfunction() {
+   if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+   } else {
+      navbar.classList.remove("sticky");
+   }
+}
 
 
