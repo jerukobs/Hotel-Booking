c9dd974b0e4b4f8a89bf18e8d05c306c9b127b88
Your Name <you@example.com>
2019-04-30 22:20:36 +0100

    changed colours :)

---
+++ login.php
@@ -0,0 +1,44 @@
+<?php
+
+	//including the server connection string
+		//include 'logon.php';	
+
+// checking whether user click on save 
+		if (isset($_POST['submit'])) {
+
+// saving values on the form into the table
+		     $studentid= $_POST['studentid'];
+		     $pin= $_POST['pin'];
+		   
+
+		   /* $sql= " INSERT INTO student (memberid, firstname, lastname, age, city, region, comment) 
+		    		VALUES ('$memberid','$firstname','$lastname','$age', '$city', '$region', '$comment')";
+
+		    $results= mysqli_query($conn,$sql);
+		    if ($results) {
+		    	echo "Record has been successfully added";
+		    }
+		  	
+		  } */
+	?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
